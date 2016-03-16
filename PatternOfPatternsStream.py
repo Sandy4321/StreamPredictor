4bc43cab04b8c2c7defc14be9aef94f8e4358473
abhishekraok <abhishkerao5002@gmail.com>
2016-03-16 09:28:31 -0700

    minor modifications, adding iterations in main

--- PatternOfPatternsStream.py
+++ PatternOfPatternsStream.py
@@ -2,17 +2,24 @@
 Pattern of patterns.
 
 Kind of adaptive coding.
+
+Todos:
+1. graphs
+2. Multi step look ahead
 """
 import numpy as np
 import time
+import os
 
 # constants
-starting_strength = 1000
-maxlen_word = 20
-feed_strength_gain = 2048
+starting_strength = 10000
+maxlen_word = 100
+feed_strength_gain = 2048 * 8
 decay_strength_loss = 1
 max_input_stream_length = 1000000
 feed_ratio = [0.6, 0.2, 0.2]
+storage_file = 'patterns_of_pride_overnight.csv'
+input_txt_file = 'data/pride.txt'
 
 
 class Pop:
@@ -21,7 +28,7 @@ class Pop:
         self.strength = starting_strength
         self.first_component = None
         self.second_component = None
-        # print 'Created pop ' + self.unrolled_pattern
+        print 'Created pop ' + self.unrolled_pattern
 
     def set_components(self, first_component, second_component):
         if not first_component or not second_component:
@@ -77,29 +84,40 @@ class PopManager:
                 current_word = string[i:i + j]
                 if current_word in self.patterns_collection:
                     current_pop = self.patterns_collection[current_word]
-                    self.patterns_collection[current_word].feed(feed_strength_gain)
+                    # self.patterns_collection[current_word].feed(feed_strength_gain)
                     i += j
                     new_pattern = previous_pop.unrolled_pattern + current_word
                     if new_pattern not in self.patterns_collection:
                         self.patterns_collection[new_pattern] = Pop(new_pattern)
                         self.patterns_collection[new_pattern].set_components(previous_pop, current_pop)
+                    else:
+                        self.patterns_collection[new_pattern].feed(feed_strength_gain)
                     previous_pop = current_pop
                     break
             if i % 10 == 0 and i > starting_strength:
                 self.cull(0)
-            self.cull(starting_strength)
+        self.cull(0)
         return self.patterns_collection
 
     def cull(self, limit):
         cull_list = []
-        for key, ngram in self.patterns_collection.iteritems():
-            ngram.decay()
-            if ngram.strength < limit and len(ngram.unrolled_pattern) > 1:
-                cull_list.append(key)
+        for key, pop in self.patterns_collection.iteritems():
+            pop.decay()
+            self.cull_child_and_mark_self(cull_list, key, limit, pop)
         for cull_key in cull_list:
-            # print 'Culling ' + cull_key
+            print 'Culling ' + cull_key
             self.patterns_collection.pop(cull_key)
-            # Todo how ot remove components?
+            # Todo how to remove components?
+
+    def cull_child_and_mark_self(self, cull_list, key, limit, pop):
+        if pop.first_component:
+            if pop.first_component.strength < limit:
+                pop.first_component = None
+        if pop.second_component:
+            if pop.second_component.strength < limit:
+                pop.second_component = None
+        if pop.strength < limit and len(pop.unrolled_pattern) > 1:
+            cull_list.append(key)
 
     def status(self):
         for key, ngram in sorted(self.patterns_collection.iteritems(), key=lambda ng: ng[1].strength):
@@ -163,21 +181,24 @@ class PopManager:
         return generated_output
 
     def generate_default(self):
-        self.load('patterns_of_pride.csv')
+        self.load(storage_file)
         output = self.generate_stream(100, 'The')
         print output
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    test_string = 'abfacdabcadadabcdabcdafabcdabcdevabcdabcdadfaabcd;kjojafaabcd'
-    with open('data/pride.txt', 'r') as myfile:
-        data = myfile.read().replace('\n', '').replace(' ', '_')
-        data = data[:max_input_stream_length]
-        data = ''.join(e for e in data if e.isalnum() or e is '_')
-    pm = PopManager()
-    pm.train(data)
-    # pm.status()
-    pm.save('patterns_of_pride.csv')
-    pm.generate_default()
-    print 'Total time taken to run this program is ', round((time.time() - start_time) / 60, ndigits=2), ' mins'
+    for iteration in range(100):
+        print ' Iteration number ' + str(iteration)
+        start_time = time.time()
+        with open(input_txt_file, 'r') as myfile:
+            data = myfile.read().replace('\n', '').replace(' ', '_')
+            data = data[:max_input_stream_length]
+            data = ''.join(e for e in data if e.isalnum() or e is '_')
+        pm = PopManager()
+        if os.path.isfile(storage_file):
+            pm.load(storage_file)
+        pm.train(data)
+        # pm.status()
+        pm.save(storage_file)
+        pm.generate_default()
+        print 'Total time taken to run this program is ', round((time.time() - start_time) / 60, ndigits=2), ' mins'
