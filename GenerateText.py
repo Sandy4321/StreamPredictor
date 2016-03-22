0ffc16a1747e37038ce3387da25ee5eb3c5a4498
abhishekraok <abhishkerao5002@gmail.com>
2016-03-21 23:10:48 -0700

    fixed generateText.py, printing result. Added web.config

--- GenerateText.py
+++ GenerateText.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     pm = PopManager()
     pm.load('PatternStore/HarryPotter.tsv')
     seed =raw_input('Enter seed word for generation')
-    pm.generate_stream(100, seed=seed)
+    print pm.generate_stream(100, seed=seed)
