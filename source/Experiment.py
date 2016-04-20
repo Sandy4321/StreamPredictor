36a61070a3bf0172d8cad82cfb48e86a1d650ca3
abhishekraok <abhishek.rao.grad@gmail.com>
2016-04-20 09:41:21 -0700

    removed class Experiment

--- source/Experiment.py
+++ source/Experiment.py
@@ -79,73 +79,74 @@ def sanity_check_run():
     print 'Everything OK'
 
 
-class Experiment:
-    def perplexity_experiment(self, string):
-        words = nltk.word_tokenize(string)
-        word_count = len(words)
-        train_count = int(0.99 * word_count)
-        train_words = words[:train_count]
-        test_words = words[train_count:]
-        sp = StreamPredictor.StreamPredictor()
-        sp.pop_manager.train_token(train_words)
-        sp.file_manager.save_pb_plain('../PatternStore/pride_token2.txt')
-        perplexity_list = sp.pop_manager.calculate_perplexity(test_words)
-        plt.plot(perplexity_list)
+def perplexity_experiment(string):
+    words = nltk.word_tokenize(string)
+    word_count = len(words)
+    train_count = int(0.99 * word_count)
+    train_words = words[:train_count]
+    test_words = words[train_count:]
+    sp = StreamPredictor.StreamPredictor()
+    sp.pop_manager.train_token(train_words)
+    sp.file_manager.save_pb_plain('../PatternStore/pride_token2.txt')
+    perplexity_list = sp.pop_manager.calculate_perplexity(test_words)
+    plt.plot(perplexity_list)
+    plt.show()
+
+
+def perplexity_experiment_load(string):
+    words = nltk.word_tokenize(string)
+    word_count = len(words)
+    train_count = int(0.99 * word_count)
+    test_words = words[train_count:]
+    sp = StreamPredictor.StreamPredictor()
+    sp.file_manager.load_pb_plain('../PatternStore/pride_token.txt')
+    perplexity_list = sp.pop_manager.calculate_perplexity(test_words)
+    plt.plot(perplexity_list)
+    plt.show()
+
+
+def train_and_perplexity():
+    words = DataObtainer.get_clean_words_from_file('../Data/pride.txt', 10 ** 9)
+    sp = StreamPredictor.StreamPredictor()
+    perplexity_list, x_list = sp.pop_manager.train_token_and_perplexity(words)
+    plt.plot(x_list, perplexity_list)
+    plt.show()
+
+
+def generalize_token():
+    sp = StreamPredictor.StreamPredictor()
+    words = DataObtainer.get_clean_words_from_file('../Data/pride.txt', 10 ** 9)
+    sp.pop_manager.train_token(words)
+    sp.generalizer.generalize()
+    sp.file_manager.save_pb_plain('../PatternStore/pride_generalized.txt')
+
+
+def online_token_perplexity_trainer():
+    print 'Starting online training with tokens and perplexity calculation'
+    sp = StreamPredictor.StreamPredictor()
+    if os.path.isfile(storage_file):
+        sp.file_manager.load_pb(storage_file)
+        print 'Loaded PopManager.PopManager from ', storage_file
+    else:
+        print ' Created new PopManager.PopManager. Didnt find anything at ', storage_file
+    for iteration in range(10):
+        start_time = time.time()
+        print 'Iteration number ' + str(iteration)
+        words = DataObtainer.get_online_words(10 ** 10)
+        perplexity_over_training, training_time = sp.pop_manager.train_token_and_perplexity(words)
+        plt.plot(training_time, perplexity_over_training, 'd-')
         plt.show()
-
-    def perplexity_experiment_load(self, string):
-        words = nltk.word_tokenize(string)
-        word_count = len(words)
-        train_count = int(0.99 * word_count)
-        test_words = words[train_count:]
-        sp = StreamPredictor.StreamPredictor()
-        sp.file_manager.load_pb_plain('../PatternStore/pride_token.txt')
-        perplexity_list = sp.pop_manager.calculate_perplexity(test_words)
-        plt.plot(perplexity_list)
-        plt.show()
-
-    @staticmethod
-    def train_and_perplexity():
-        words = DataObtainer.get_clean_words_from_file('../Data/pride.txt', 10 ** 9)
-        sp = StreamPredictor.StreamPredictor()
-        perplexity_list, x_list = sp.pop_manager.train_token_and_perplexity(words)
-        plt.plot(x_list, perplexity_list)
-        plt.show()
-
-    @staticmethod
-    def generalize_token():
-        sp = StreamPredictor.StreamPredictor()
-        words = DataObtainer.get_clean_words_from_file('../Data/pride.txt', 10 ** 9)
-        sp.pop_manager.train_token(words)
-        sp.generalizer.generalize()
-        sp.file_manager.save_pb_plain('../PatternStore/pride_generalized.txt')
-
-    @staticmethod
-    def online_token_perplexity_trainer():
-        print 'Starting online training with tokens and perplexity calculation'
-        sp = StreamPredictor.StreamPredictor()
-        if os.path.isfile(storage_file):
-            sp.file_manager.load_pb(storage_file)
-            print 'Loaded PopManager.PopManager from ', storage_file
-        else:
-            print ' Created new PopManager.PopManager. Didnt find anything at ', storage_file
-        for iteration in range(10):
-            start_time = time.time()
-            print 'Iteration number ' + str(iteration)
-            words = DataObtainer.get_online_words(10 ** 10)
-            perplexity_over_training, training_time = sp.pop_manager.train_token_and_perplexity(words)
-            plt.plot(training_time, perplexity_over_training,'d-')
-            plt.show()
-            sp.file_manager.save_pb(storage_file)
-            total_time_mins = (time.time() - start_time) / 60
-            rate_words_min = round(len(words) / total_time_mins / 1000)
-            print 'Total time taken to run this is ', round(total_time_mins, ndigits=2), \
-                ' mins. Rate = ', rate_words_min, ' K words/min'
+        sp.file_manager.save_pb(storage_file)
+        total_time_mins = (time.time() - start_time) / 60
+        rate_words_min = round(len(words) / total_time_mins / 1000)
+        print 'Total time taken to run this is ', round(total_time_mins, ndigits=2), \
+            ' mins. Rate = ', rate_words_min, ' K words/min'
 
 
 if __name__ == '__main__':
     # experiment.perplexity_experiment(text)
     # experiment.perplexity_experiment(text)
-    sanity_check_run()
-    Experiment.train_and_perplexity()
+    # sanity_check_run()
+    # Experiment.train_and_perplexity()
+    train_and_perplexity()
     # Experiment.generalize_token()
